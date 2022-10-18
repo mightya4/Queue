832baa87ddc80860029ba98e82c1c13fb978c77d
Akeem <akeem.mighty@gmail.com>
2022-10-17 23:24:53 -0400

    Create Queue Books Object + Add Book to Books Queue

--- JavaQueue.java
+++ JavaQueue.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class JavaQueue {
     public static void main(String[] args) {
-        
+        Queue<String> books = new Queue<String>();
+        books.add("To Kill a Mockingbird");
+        books.add("The Hunger Games");
+        books.add("The Da Vinci Code");
+        books.add("Water for Elephants");
+        books.add("Lord of the Flies");
+
     }
 }
