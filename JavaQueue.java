714c1be352fecebf482c09320997c959021ef7b5
Akeem <akeem.mighty@gmail.com>
2022-10-18 22:31:14 -0400

    Imported Necessary Class PriortyQueue and Queue + Removed First Element From Queue

--- JavaQueue.java
+++ JavaQueue.java
@@ -1,13 +1,23 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class JavaQueue {
     public static void main(String[] args) {
-        Queue<String> books = new Queue<String>();
+        Queue<String> books = new PriorityQueue<String>();
+
+
         books.add("To Kill a Mockingbird");
         books.add("The Hunger Games");
         books.add("The Da Vinci Code");
         books.add("Water for Elephants");
         books.add("Lord of the Flies");
+
+        System.out.println(books);
+
+        //Remove First Element in Queue
+        books.remove();
+
+        System.out.println(books);
 
     }
 }
